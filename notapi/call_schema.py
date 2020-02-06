2a272045523f416ffef5daf96756c82bd7547e1c
Raphael Bialon <raphael.bialon@uni-duesseldorf.de>
2020-02-06 13:28:18 +0100

    Fix schema validator
    Instead of returning boolean values, marshmallow expects exceptions on
    validation errors. My bad.

--- notapi/call_schema.py
+++ notapi/call_schema.py
@@ -1,4 +1,4 @@
-from marshmallow import INCLUDE, Schema, fields, validates
+from marshmallow import INCLUDE, Schema, ValidationError, fields, validates
 
 EVENT_NEWCALL = "newCall"
 EVENT_ANSWER = "answer"
@@ -9,7 +9,7 @@ SUPPORTED_EVENTS = (EVENT_NEWCALL, EVENT_ANSWER, EVENT_HANGUP)
 class CallSchema(Schema):
     class Meta:
         unknown = INCLUDE
-        
+
     event = fields.Str(required=True)
     phone_from = fields.Str(required=True, data_key="from")
     phone_to = fields.Str(required=True, data_key="to")
@@ -17,8 +17,18 @@ class CallSchema(Schema):
 
     @validates("event")
     def is_event_supported(self, event):
-        return event in SUPPORTED_EVENTS
+        if event not in SUPPORTED_EVENTS:
+            raise ValidationError(f"event {event} not supported")
+
+    @validates("phone_from")
+    @validates("phone_to")
+    def is_phone_valid(self, phone):
+        valid = (0 < len(phone) <= 50) and phone.isnumeric()
+
+        if not valid:
+            raise ValidationError(f"phone number {phone} not valid")
 
     @validates("direction")
     def is_valid_direction(self, direction):
-        return direction in ("in", "out")
+        if direction not in ("in", "out"):
+            raise ValidationError(f"direction {direction} not supported")
